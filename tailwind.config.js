e335729dedc89ae4af40ebdcc9bb5ebf4551971e
Mathangi Pushparajah <mathangi23@outlook.com>
2024-09-16 17:32:05 +0200

    Add color scheme in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-green": "#005031",
+        "light-green": "#25664e",
+      },
+    },
   },
   plugins: [],
   corePlugins: {
