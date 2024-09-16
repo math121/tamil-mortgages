2b5a59abd87cf1b7c9c540b74e2ed602c5765db3
Mathangi Pushparajah <mathangi23@outlook.com>
2024-09-16 18:33:48 +0200

    Add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/**/*.{js,jsx}",
+  ],
   theme: {
     extend: {
       colors: {
         "dark-green": "#005031",
         "light-green": "#25664e",
+      },
+      fontFamily: {
+        forum: ['"Forum"', ...defaultTheme.fontFamily.sans],
+        nunito: ['"Nunito"', ...defaultTheme.fontFamily.sans],
       },
     },
   },
