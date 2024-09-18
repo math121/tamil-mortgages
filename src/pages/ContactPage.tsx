ec236795f35aeb071ff09fa282a76b6bc6708491
Mathangi Pushparajah <mathangi23@outlook.com>
2024-09-18 15:43:43 +0200

    add contact details

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -8,22 +8,20 @@ export const ContactPage = () => {
       />
       <div className="lg:self-center">
         <h1>Contact us</h1>
-        <div className="grid grid-cols-2 lg:grid-cols-1">
-          <div>
-            <label className="font-semibold">Address:</label>
-            <address className="mb-4">
-              Lorem ipsum, <br />
-              Dolores ipsa, <br />
-              exercitationem
-            </address>
-          </div>
-          <div>
-            <label className="font-semibold">Email address:</label>
-            <p className="mt-0">Lorem@ipsum.com</p>
-            <label className="font-semibold">Phone Number:</label>
-            <p className="my-0">#########</p>
-          </div>
-        </div>
+        <label className="font-semibold">Address:</label>
+        <address className="mb-4">
+          All Credit Mortgages
+          <br />
+          Lansbury drive
+          <br />
+          London
+          <br />
+          UB4 8SD
+        </address>
+        <label className="font-semibold">Email address:</label>
+        <p className="mt-0">info@allcreditmortgages.co.uk</p>
+        <label className="font-semibold">Phone Number:</label>
+        <p className="my-0">07799752777</p>
       </div>
       <img
         className="object-cover h-56 w-full pt-9 lg:hidden"
