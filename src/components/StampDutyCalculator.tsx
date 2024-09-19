9459fa0fd3508adb8e3064ff03458d2407a7384a
Mathangi Pushparajah <mathangi23@outlook.com>
2024-09-19 16:51:57 +0200

    Fix stamp duty calculator layout for desktop

--- src/components/StampDutyCalculator.tsx
+++ src/components/StampDutyCalculator.tsx
@@ -34,12 +34,15 @@ export const StampDutyCalculator = () => {
   };
 
   return (
-    <div className="p-2">
-      <p>
-        Specify the property price in pounds and tick the box if you are a first
-        time buyer or a second time buyer/ buying additional properties
-      </p>
-      <form onSubmit={handleSubmit(calculateStampDuty)} className="space-y-3">
+    <div className="p-2 md:flex md:gap-10">
+      <form
+        onSubmit={handleSubmit(calculateStampDuty)}
+        className="space-y-3 md:w-2/3"
+      >
+        <p>
+          Specify the property price in pounds and tick the box if you are a
+          first time buyer or a second time buyer/ buying additional properties
+        </p>
         <label className="font-semibold">Property price:</label>
         <div className="flex">
           <span className="inline-flex items-center px-4 text-sm bg-lightest-green rounded-s-md font-bold">
@@ -53,7 +56,7 @@ export const StampDutyCalculator = () => {
           />
         </div>
 
-        <fieldset className="border-none p-0 pt-4 flex justify-around gap-4">
+        <fieldset className="border-none p-0 pt-4 flex justify-normal gap-4">
           <div className="flex items-center mb-4">
             <input
               id="firstbuy"
@@ -82,22 +85,20 @@ export const StampDutyCalculator = () => {
         </fieldset>
         <button
           type="submit"
-          className="p-2 w-2/6 rounded-full text-white bg-dark-green cursor-pointer hover:bg-light-green"
+          className="p-2 w-48 rounded-full text-white bg-dark-green cursor-pointer hover:bg-light-green sm:w-60"
         >
           Calculate
         </button>
       </form>
 
       {result && rate && (
-        <>
+        <div>
           <section className="border-2 border-solid border-dark-green rounded-xl px-2 mt-10">
-            <div className="gap-3 items-center">
-              <p>Stamp duty on your first property/additional property is</p>
-              <p className="font-bold text-2xl my-0">{result}</p>
-              <p>Rate: {rate}</p>
-            </div>
+            <p>Stamp duty on your first property/additional property is</p>
+            <p className="font-bold text-2xl my-0">{result}</p>
+            <p>Rate: {rate}</p>
           </section>
-        </>
+        </div>
       )}
     </div>
   );
