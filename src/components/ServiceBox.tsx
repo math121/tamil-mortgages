f7cf30ec94405f651097031419c78feeb3b584c1
Mathangi Pushparajah <mathangi23@outlook.com>
2024-09-20 15:16:21 +0200

    Make Service info boxes expand and collapse on click in mobile view

--- src/components/ServiceBox.tsx
+++ src/components/ServiceBox.tsx
@@ -1,22 +1,52 @@
+import { ServiceBoxInfo } from "../utils/types";
+
 export const ServiceBox = ({
-  title,
-  text,
+  info,
+  active,
+  handleExpandCollapse,
 }: {
-  title: string;
-  text: string;
+  info: ServiceBoxInfo;
+  active: number;
+  handleExpandCollapse: (id: number) => void;
 }) => {
   return (
     <>
-      <section className="border-2 border-solid border-dark-green rounded-[40px] px-4">
-        <div className="flex gap-3 items-center">
-          <img
-            className="rounded-full border-solid w-6 h-6"
-            src="/src/assets/services-tick.png"
-            alt="green tick icon"
-          />
-          <p className="uppercase">{title}</p>
+      <section
+        className="border-2 border-solid border-dark-green rounded-[40px] px-4"
+        onClick={() => handleExpandCollapse(info.id)}
+      >
+        <div className="flex items-center justify-between">
+          <div className="flex gap-3 items-center">
+            <img
+              className="rounded-full border-solid w-6 h-6"
+              src="/src/assets/services-tick.png"
+              alt="green tick icon"
+            />
+            <p className="uppercase">{info.title}</p>
+          </div>
+          <svg
+            data-accordion-icon
+            className={`w-4 h-4 shrink-0 sm:hidden ${info.id === active ? `rotate-0` : `rotate-180`}`}
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 10 6"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M9 5 5 1 1 5"
+            />
+          </svg>
         </div>
-        <p className="mt-0">{text}</p>
+
+        <p
+          className={`mt-0 ${info.id === active ? `block` : `hidden`} sm:block`}
+        >
+          {info.text}
+        </p>
       </section>
     </>
   );
---
+++ src/utils/types.ts
@@ -0,0 +1,5 @@
+export type ServiceBoxInfo = {
+  id: number;
+  title: string;
+  text: string;
+};
