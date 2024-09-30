ec2c0f04fcc2b9669e4b261440b78b0ffd65c419
Mathangi Pushparajah <mathangi23@outlook.com>
2024-09-30 21:47:27 +0200

    Change green ticks to orange

--- src/components/ServiceBox.tsx
+++ src/components/ServiceBox.tsx
@@ -16,13 +16,13 @@ export const ServiceBox = ({
   return (
     <>
       <section
-        className="border-2 border-solid border-dark-green rounded-[40px] px-4"
+        className="border-2 border-solid border-dark-orange rounded-[40px] px-4"
         onClick={() => handleExpandCollapse(info.id)}
       >
         <div className="flex items-center justify-between">
           <div className="flex gap-3 items-center">
             <img
-              className="rounded-full border-solid w-6 h-6"
+              className="rounded-full w-8 h-8"
               src="/src/assets/services-tick.png"
               alt="green tick icon"
             />
